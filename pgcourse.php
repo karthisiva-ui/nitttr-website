c89afcffa74fbed30d51b94adb64478a0987246e
Dharan Gokul R <64479986+DharanGokul@users.noreply.github.com>
2024-11-09 12:22:46 +0530

    Admissions pages responsiveness

--- pgcourse.php
+++ pgcourse.php
@@ -18,86 +18,101 @@
   <ol>
     <li><a href="index.php">Home</a></li>
     <li class="vision">Academics</li> 
-    <li class="vision">courses Offered</li> 
+    <li class="vision">Courses Offered</li> 
   </ol>
 </div>
 </nav>
 </div><!-- End Page Title -->
 
 
-<div class="container-fluid justify-content-center" data-aos="fade-up">
+<div class="container justify-content-center py-5 course-list" data-aos="fade-up">
 
-        <div class="row" style="text-align: center;">
-        <br/>
-        <div class="col-md-1">&nbsp;</div>
-        <div class="card col-md-3 mb-4">
-            <img class="card-img-top" src="https://www.nitttrc.ac.in/admissions/cse.JPG" height="200" width="300" alt="Card image">
-            <div class="card-body">
-                <h4 class="card-text">M.Tech ARTIFICIAL INTELLIGENCE & MACHINE LEARNING</h4>
-                <a href="https://www.nitttrc.ac.in/admissions/AI&ML.pdf" class="btn btn-primary" target="_blank">Download PDF</a>
+        <div class="row justify-content-center" style="text-align: center;">
+        <!-- <div class="col-md-1">&nbsp;</div> -->
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img class="card-img-top" src="https://www.nitttrc.ac.in/admissions/cse.JPG" height="200" width="300" alt="Card image">
+                <div class="card-body">
+                    <h4 class="card-text">M.Tech ARTIFICIAL INTELLIGENCE & MACHINE LEARNING</h4>
+                    <a href="https://www.nitttrc.ac.in/admissions/AI&ML.pdf" class="btn btn-primary" target="_blank">Download PDF</a>
+                </div>
             </div>
         </div>
 
-        <div class="card col-md-3">
-            <img class="card-img-top" src="https://www.nitttrc.ac.in/admissions/arvr.JPG" height="200" width="300" alt="Card image">
-            <div class="card-body">
-                <h4 class="card-text">M. Tech. AUGMENTED AND VIRTUAL REALITY</h4>
-                <a href="https://www.nitttrc.ac.in/admissions/AR &VR.pdf" class="btn btn-primary" target="_blank">Download PDF</a>
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img class="card-img-top" src="https://www.nitttrc.ac.in/admissions/arvr.JPG" height="200" width="300" alt="Card image">
+                <div class="card-body">
+                    <h4 class="card-text">M. Tech. AUGMENTED AND VIRTUAL REALITY</h4>
+                    <a href="https://www.nitttrc.ac.in/admissions/AR &VR.pdf" class="btn btn-primary" target="_blank">Download PDF</a>
+                </div>
             </div>
         </div>
         
-        <div class="card col-md-3">
-            <img class="card-img-top" src="https://www.nitttrc.ac.in/admissions/education.JPG" height="200" width="300" alt="Card image">
-            <div class="card-body">
-                <h4 class="card-text">M.Tech. ENGINEERING EDUCATION</h4><br>
-                <a href="https://www.nitttrc.ac.in/admissions/EDUCATION.pdf" class="btn btn-primary" target="_blank">Download PDF</a>
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img class="card-img-top" src="https://www.nitttrc.ac.in/admissions/education.JPG" height="200" width="300" alt="Card image">
+                <div class="card-body">
+                    <h4 class="card-text">M.Tech. ENGINEERING EDUCATION</h4><br>
+                    <a href="https://www.nitttrc.ac.in/admissions/EDUCATION.pdf" class="btn btn-primary" target="_blank">Download PDF</a>
+                </div>
             </div>
         </div>
     </div>
 
-<div class="row" style="text-align: center;">
-    <div class="col-md-1">&nbsp;</div>
-        <div class="card col-md-3">
-            <img class="card-img-top" src="https://www.nitttrc.ac.in/admissions/civil.JPG" height="200" width="300" alt="Card image">
-            <div class="card-body">
-                <h4 class="card-text">M. Tech. INFRASTRUCTURE ENGINEERING AND MANAGEMENT</h4>
-                <a href="https://www.nitttrc.ac.in/admissions/CIVIL.pdf" class="btn btn-primary" target="_blank">Download PDF</a>
+<div class="row justify-content-center" style="text-align: center;">
+    <!-- <div class="col-md-1">&nbsp;</div> -->
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img class="card-img-top" src="https://www.nitttrc.ac.in/admissions/civil.JPG" height="200" width="300" alt="Card image">
+                <div class="card-body">
+                    <h4 class="card-text">M. Tech. INFRASTRUCTURE ENGINEERING AND MANAGEMENT</h4>
+                    <a href="https://www.nitttrc.ac.in/admissions/CIVIL.pdf" class="btn btn-primary" target="_blank">Download PDF</a>
+                </div>
             </div>
         </div>
 
-        <div class="card col-md-3">
-            <img class="card-img-top" src="https://www.nitttrc.ac.in/admissions/industry.JPG" height="200" width="300" alt="Card image">
-            <div class="card-body">
-                <h4 class="card-text">M. Tech. MECHATRONICS</h4><p style="height:20px;">&nbsp;</p>
-                <a href="https://www.nitttrc.ac.in/admissions/MECHATRONICS.pdf" class="btn btn-primary" target="_blank">Download PDF</a>
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img class="card-img-top" src="https://www.nitttrc.ac.in/admissions/industry.JPG" height="200" width="300" alt="Card image">
+                <div class="card-body">
+                    <h4 class="card-text">M. Tech. MECHATRONICS</h4><p style="height:20px;">&nbsp;</p>
+                    <a href="https://www.nitttrc.ac.in/admissions/MECHATRONICS.pdf" class="btn btn-primary" target="_blank">Download PDF</a>
+                </div>
             </div>
         </div>
 
-        <div class="card col-md-3">
-            <img class="card-img-top" src="https://www.nitttrc.ac.in/admissions/pe.JPG" height="200" width="300" alt="Card image">
-            <div class="card-body">
-                <h4 class="card-text">M. Tech. POWER ELECTRONICS AND DRIVES</h4>
-                <a href="https://www.nitttrc.ac.in/admissions/POWER ELECTRONICS.pdf" class="btn btn-primary" target="_blank" >Download PDF</a>
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img class="card-img-top" src="https://www.nitttrc.ac.in/admissions/pe.JPG" height="200" width="300" alt="Card image">
+                <div class="card-body">
+                    <h4 class="card-text">M. Tech. POWER ELECTRONICS AND DRIVES</h4>
+                    <a href="https://www.nitttrc.ac.in/admissions/POWER ELECTRONICS.pdf" class="btn btn-primary" target="_blank" >Download PDF</a>
+                </div>
             </div>
         </div>
         </div>
 
-<div class="row" style="text-align: center;">
-    <div class="col-md-1">&nbsp;</div>
+<div class="row justify-content-center" style="text-align: center;">
+    <!-- <div class="col-md-1">&nbsp;</div> -->
 
-        <div class="card col-md-3" >
-            <img class="card-img-top" src="https://www.nitttrc.ac.in/admissions/VLSI.JPG" height="200" width="300" alt="Card image">
-            <div class="card-body">
-                <h4 class="card-text">M. Tech. VLSI DESIGN AND EMBEDDED SYSTEMS</h4>
-                <a href="https://www.nitttrc.ac.in/admissions/VLSI.pdf" class="btn btn-primary" target="_blank">Download PDF</a>
+        <div class="col-md-4 mb-4" >
+            <div class="card">
+                <img class="card-img-top" src="https://www.nitttrc.ac.in/admissions/VLSI.JPG" height="200" width="300" alt="Card image">
+                <div class="card-body">
+                    <h4 class="card-text">M. Tech. VLSI DESIGN AND EMBEDDED SYSTEMS</h4>
+                    <a href="https://www.nitttrc.ac.in/admissions/VLSI.pdf" class="btn btn-primary" target="_blank">Download PDF</a>
+                </div>
             </div>
         </div>
 
-        <div class="card col-md-3">
-            <img class="card-img-top" src="https://www.nitttrc.ac.in/admissions/mba.JPG" height="200" width="300" alt="Card image">
-            <div class="card-body">
-                <h4 class="card-text">M.B.A. (BUSINESS ANALYTICS)</h4><p style="height:15px;">&nbsp;</p>
-                <a href="https://www.nitttrc.ac.in/admissions/MBA.pdf" class="btn btn-primary" target="_blank">Download PDF</a>
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img class="card-img-top" src="https://www.nitttrc.ac.in/admissions/mba.JPG" height="200" width="300" alt="Card image">
+                <div class="card-body">
+                    <h4 class="card-text">M.B.A. (BUSINESS ANALYTICS)</h4><p style="height:15px;">&nbsp;</p>
+                    <a href="https://www.nitttrc.ac.in/admissions/MBA.pdf" class="btn btn-primary" target="_blank">Download PDF</a>
+                </div>
             </div>
         </div>
     </div>
@@ -112,20 +127,14 @@
 
 
 <style>
-
-   
-   .card {
-        margin: 10px;
-    }
-
     .card img {
         width: 100%;
     }
 
-    .card-body {
+    /* .card-body {
         padding: 10px;
         height: 100px;
-    }
+    } */
 
    
 </style>
